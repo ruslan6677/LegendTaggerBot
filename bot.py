afc3189127ff47a43f1e2202f49ca2610f9c71d6
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2022-12-28 20:09:54 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ async def help(event):
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
   if event.is_private:
-    async for usr in client.iter_participants(event.chat_id):
+   async for usr in client.iter_participants(event.chat_id):
      ad = f"[{usr.first_name}](tg://user?id={usr.id}) "
      await client.send_message(log_qrup, f"ℹ️ **Yeni istifadəçi -** {ad}")
      return await event.reply("salam gijdillax",	
